f3bf43eef2edfe64703de1ca279aec2631ba9e87
Ruthellon <58911878+Ruthellon@users.noreply.github.com>
2020-03-11 00:19:52 -0400

    Added CharacterInfo updater to app

--- InitiativeApp/InitiativeApp/MainWindow.xaml.cs
+++ InitiativeApp/InitiativeApp/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InitiativeApp
 {
@@ -20,12 +15,99 @@ namespace InitiativeApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Tuple<string, string>> characterList = new List<Tuple<string, string>>() {
+            new Tuple<string, string>("Hella", "https://www.dndbeyond.com/character/22078134/json"),
+            new Tuple<string, string>("Leedle", "https://www.dndbeyond.com/character/22081606/json"),
+            new Tuple<string, string>("Key", "https://www.dndbeyond.com/character/22960105/json"),
+            new Tuple<string, string>("Jon", "https://www.dndbeyond.com/character/21973481/json"),
+            new Tuple<string, string>("Shazlight", "https://www.dndbeyond.com/character/21211796/json")};
+
         private List<Character> Characters = new List<Character>();
+        private static System.Timers.Timer localTimer;
 
         public MainWindow()
         {
             InitializeComponent();
             lstCharacters.ItemsSource = Characters;
+            localTimer = new System.Timers.Timer(1000);
+            localTimer.Elapsed += LocalTimer_Elapsed;
+            localTimer.AutoReset = true;
+            localTimer.Enabled = true;
+        }
+
+        private bool firstRun = true;
+        private void LocalTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (firstRun)
+            {
+                localTimer.Interval = 120000;
+                firstRun = false;
+            }
+
+            foreach (var character in characterList)
+            {
+                int maxHealth = 0;
+                int constitution = 0;
+                int level = 0;
+                int removedPoints = 0;
+                bool hasInspiration = false;
+
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead(character.Item2))
+                using (var reader = new StreamReader(stream))
+                {
+                    var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+
+                    maxHealth = (int)jObject.SelectToken("baseHitPoints");
+                    constitution = (int)jObject.SelectToken("stats")[2].SelectToken("value");
+                    level = (int)jObject.SelectToken("classes")[0].SelectToken("level");
+                    removedPoints = (int)jObject.SelectToken("removedHitPoints");
+                    hasInspiration = (bool)jObject.SelectToken("inspiration");
+
+                    foreach (var item in jObject.SelectToken("modifiers").SelectToken("race"))
+                    {
+                        string subtype = (string)item.SelectToken("subType");
+                        if (subtype.Contains("constitution"))
+                        {
+                            constitution += (int)item.SelectToken("value");
+                        }
+                    }
+                }
+
+                maxHealth = (maxHealth + (((constitution / 2) - 5) * level));
+
+                using (StreamWriter write = new StreamWriter("C:\\DnDStuff\\" + character.Item1 + "Health.txt"))
+                {
+                    write.WriteLine(maxHealth);
+                }
+
+                using (StreamWriter write = new StreamWriter("C:\\DnDStuff\\" + character.Item1 + "HealthCurrent.txt"))
+                {
+                    write.WriteLine((maxHealth - removedPoints));
+                }
+
+                using (StreamWriter write = new StreamWriter("C:\\DnDStuff\\" + character.Item1 + "Inspiration.txt"))
+                {
+                    if (hasInspiration)
+                        write.WriteLine("INS");
+                    else
+                        write.WriteLine("");
+                }
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtBlock.Text += "Char: " + character.Item1 + " ( " + level + " )" + Environment.NewLine;
+                    txtBlock.Text += "     HP: " + (maxHealth - removedPoints) + " / " + maxHealth + Environment.NewLine;
+                    txtBlock.Text += "     Inspiration: " + hasInspiration + Environment.NewLine;
+                });
+
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            this.Dispatcher.Invoke(() =>
+            {
+                txtBlock.Text += Environment.NewLine + DateTime.Now.ToLongTimeString() + ": Retrieved" + Environment.NewLine;
+            });
         }
 
         private void BtnAddChar_Click(object sender, RoutedEventArgs e)
